98d775cbee0da6d2f354446a760a94874e0325fd
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-01-20 21:35:52 -0800

    modify math function, now sums with '=', can also change operator

--- calcapp.js
+++ calcapp.js
@@ -7,9 +7,10 @@ const mathBtns = document.querySelectorAll('.mathFnc');
 const allBtns = document.querySelectorAll('button');
 let viewport = document.querySelector('.result');
 let smallViewport = document.querySelector('.nextNum')
-let currentMathFnc = document.querySelector('.currentMathFnc')
+let mathViewport = document.querySelector('.currentMathFnc')
 let sumTotal = 0;
 let numHolder = [];
+let mathFncHolder = "";
 let num1 = [];
 let num2 = [];
 
@@ -28,31 +29,36 @@ function btnListener() {
     allBtns.forEach(button => {
         button.addEventListener('click', () => {
             if (button.textContent === "="){
-                if (numHolder.length === 0) {
-                    viewport.textContent = 0;
-                } else {
-                    viewport.textContent = numHolder.reduce((acc, val) => acc + val);
+                if (numHolder.length >= 1 && num1.length >= 1) {
+                    num2 = numHolder;
                     numHolder = [];
+                    timeToMath(num1, num2, mathFncHolder);
+                    num1 = [sumTotal.toString()];
+                    num2 = [];
                 }
             } else if (button.textContent === "Del") {
                 numHolder.pop();
                 viewport.textContent = numHolder.join("");
-                if (numHolder.length === 0){
+                if (numHolder.length === 0 && num1.length === 0){
                     viewport.textContent = 0;
+                } else if (num1.length >= 1){
+                    viewport.textContent = num1.join("");
+                    smallViewport.textContent = numHolder.join("");
                 }
             } else if (button.textContent === "Clear") {
                 numHolder = [];
                 num1 = [];
                 num2 = [];
                 viewport.textContent = 0;
-                currentMathFnc.textContent = "";
+                mathViewport.textContent = "";
                 smallViewport.textContent = "";
+                mathFncHolder = "";
             } else if (button.textContent === "Random # (1-100)") {
                 numHolder = [];
                 num1 = [];
                 num2 = [];
                 viewport.textContent = Math.floor((Math.random() * 100) + 1);
-                currentMathFnc.textContent = "";
+                mathViewport.textContent = "";
                 smallViewport.textContent = "";
             };
         })
@@ -61,67 +67,66 @@ function btnListener() {
 
 
 btnListener();
-mathTime();
+mathFncWatcher();
 
 
-function mathTime() {
+function mathFncWatcher() {
     mathBtns.forEach(button => {
         button.addEventListener('click', () => {
+            if (mathFncHolder.length > 0 && button.textContent != "="){
+                mathViewport.textContent = button.textContent;
+            }
             if (button.textContent === "*"){
-                if (num1.length === 0) {
-                    currentMathFnc.textContent = "*"
+                if (numHolder.length === 0 && num1.length >= 1) {
+                    mathFncHolder = "*";
+                    mathViewport.textContent = "*";
+                } else if (numHolder.length > 0 && num1.length > 0){
+                    mathFncHolder = "*";
+                    mathViewport.textContent = "*";
+                } else if (num1.length === 0){
+                    mathFncHolder = "*";
+                    mathViewport.textContent = "*"
                     num1 = numHolder;
-                    numHolder = [];
-                } else if (num1.length > 0 && num2.length === 0) {
-                    currentMathFnc.textContent = "*"
-                    num2 = numHolder;
-                    numHolder = [];
-                    multiply(num1, num2);
-                    num1 = [sumTotal.toString()];
-                    num2 = [];
-                    // console.log(mathHolder);
+                    numHolder = [];  
                 }
             } else if (button.textContent === "/"){
-                if (num1.length === 0) {
-                    currentMathFnc.textContent = "/"
+                if (numHolder.length === 0 && num1.length >= 1) {
+                    mathFncHolder = "/";
+                    mathViewport.textContent = "/";
+                } else if (numHolder.length > 0 && num1.length > 0){
+                    mathFncHolder = "/";
+                    mathViewport.textContent = "/"
+                } else if (num1.length === 0){
+                    mathFncHolder = "/";
+                    mathViewport.textContent = "/"
                     num1 = numHolder;
-                    numHolder = [];
-                } else if (num1.length > 0 && num2.length === 0) {
-                    currentMathFnc.textContent = "/"
-                    num2 = numHolder;
-                    numHolder = [];
-                    divide(num1, num2);
-                    num1 = [sumTotal.toString()];
-                    num2 = [];
-                    // console.log(mathHolder);
+                    numHolder = [];  
                 }
             } else if (button.textContent === "+"){
-                if (num1.length === 0) {
-                    currentMathFnc.textContent = "+"
+                if (numHolder.length === 0 && num1.length >= 1) {
+                    mathFncHolder = "+";
+                    mathViewport.textContent = "+"
+                } else if (numHolder.length > 0 && num1.length > 0){
+                    mathFncHolder = "+";
+                    mathViewport.textContent = "+"
+                } else if (num1.length === 0) {
+                    mathFncHolder = "+";
+                    mathViewport.textContent = "+"
                     num1 = numHolder;
                     numHolder = [];
-                } else if (num1.length > 0 && num2.length === 0) {
-                    currentMathFnc.textContent = "+"
-                    num2 = numHolder;
-                    numHolder = [];
-                    add(num1, num2);
-                    num1 = [sumTotal.toString()];
-                    num2 = [];
-                    // console.log(mathHolder);
                 }
             } else if (button.textContent === "-"){
-                if (num1.length === 0) {
-                    currentMathFnc.textContent = "-"
+                if (numHolder.length === 0 && num1.length >= 1) {
+                    mathFncHolder = "-";
+                    mathViewport.textContent = "-"
+                } else if (numHolder.length > 0 && num1.length > 0){
+                    mathFncHolder = "-";
+                    mathViewport.textContent = "-"
+                } else if (num1.length === 0){
+                    mathFncHolder = "-";
+                    mathViewport.textContent = "-"
                     num1 = numHolder;
                     numHolder = [];
-                } else if (num1.length > 0 && num2.length === 0) {
-                    currentMathFnc.textContent = "-"
-                    num2 = numHolder;
-                    numHolder = [];
-                    subtract(num1, num2);
-                    num1 = [sumTotal.toString()];
-                    num2 = [];
-                    // console.log(mathHolder);
                 }
             }
         })
@@ -129,99 +134,28 @@ function mathTime() {
 }
 
 
-function add(num1, num2) {
+function timeToMath(num1, num2, mathFncHolder) {
     num1 = num1.join('');
     num2 = num2.join('');
-    sumTotal = parseFloat(num1) + parseFloat(num2);
-    viewport.textContent = sumTotal;
-
-    // sumTotal = num1.reduce((acc, val) => acc + val) + num2.reduce((acc, val) => acc + val);
-    // viewport.textContent = sumTotal;
-    // num1 = sumTotal;
-}
-
-function subtract(num1, num2) {
-    num1 = num1.join('');
-    num2 = num2.join('');
-    sumTotal = parseFloat(num1) - parseFloat(num2);
-    viewport.textContent = sumTotal;
-
-    // sumTotal = num1.reduce((acc, val) => acc + val) - num2.reduce((acc, val) => acc + val);
-    // viewport.textContent = sumTotal;
-    // num1 = sumTotal;
-}
-
-function multiply(num1, num2) {
-    num1 = num1.join('');
-    num2 = num2.join('');
-    sumTotal = parseFloat(num1) * parseFloat(num2);
-    viewport.textContent = sumTotal;
-
-    // sumTotal = num1.reduce((acc, val) => acc + val) * num2.reduce((acc, val) => acc + val);
-    // viewport.textContent = sumTotal;
-    // num1 = sumTotal;
-}
-
-function divide(num1, num2) {
-    num1 = num1.join('');
-    num2 = num2.join('');
-    sumTotal = parseFloat(num1) / parseFloat(num2);
-    viewport.textContent = sumTotal;
-
-    // sumTotal = num1.reduce((acc, val) => acc + val) / num2.reduce((acc, val) => acc + val);
-    // viewport.textContent = sumTotal;
-    // num1 = sumTotal;
-}
-
-
-// store the first numbers clicked into numHolder, once you press a math function button:
-
-// num1 = numHolder; (now the first number is stored into num1)
-// numHolder = []; (resets numHolder back to empty, and readys it to receive new numbers again)
-
-// user enters more numbers.
-// if any math button, or '=' is pressed: 
-// *********************************************************************************************************
-// (should put something here if the user just starts clicking math functions, ex: they enter 2 and then 
-// click +, then -, then *, it should switch between these modes and still remember they clicked 2 as the 
-// first number) 
-// **********************************************************************************************
-
-
-// num2 = numHolder; (stores second number into num2 now)
-
-// We would now want to do the first math function and store the result into num1 again
-
-// ex. if user enters 4(-> numHolder) 4(-> numHolder) + (now num1 = numHolder and we empty numHolder = []) 2(-> numHolder) 2(-> numHolder) then * / + - or = (now num2 = numHolder)
-
-
-// viewport.textContent = num1.reduce((acc, val) => acc + val) + 'SYMBOL' + num2.reduce((acc, val) => acc + val);
-// numHolder = [];
-//
-//
-
-
-
-// ex: you enter 1 - 2 - 3 - 4
-// numHolder is now ['1', '2', '3', '4']
-
-
-// numHolder = numHolder.map(Number); // this turns numHolder into numbers
-// numHolder is now [1, 2, 3, 4]
-
-// numHolder = numHolder.join("");    // turns it into a single string
-// numHolder is now '1234'
-
-// parseInt(numHolder);              // turns string into number
-// numHolder is now 1234
-
-
-
-
-// set a counter that watches how many times a button is clicked
-// counter = 0
-// (if * is pressed), 
-// counter = 1
-// if (* is pressed && counter === 1)
-// totalSum = num1 * num1;
-// counter = 1
+    if (mathFncHolder === "*") {
+        sumTotal = parseFloat(num1) * parseFloat(num2);
+        viewport.textContent = sumTotal;
+        mathViewport.textContent = "";
+        smallViewport.textContent = "";
+    } else if (mathFncHolder === "/") {
+        sumTotal = parseFloat(num1) / parseFloat(num2);
+        viewport.textContent = sumTotal;
+        mathViewport.textContent = "";
+        smallViewport.textContent = "";
+    } else if (mathFncHolder === "+") {
+        sumTotal = parseFloat(num1) + parseFloat(num2);
+        viewport.textContent = sumTotal;
+        mathViewport.textContent = "";
+        smallViewport.textContent = "";
+    } else if (mathFncHolder === "-") {
+        sumTotal = parseFloat(num1) - parseFloat(num2);
+        viewport.textContent = sumTotal;
+        mathViewport.textContent = "";
+        smallViewport.textContent = "";
+    }
+};
